66a3503e34493db3941329c61fc8fd00cae9ef0a
Alejandro Valero <51358578+ajvulcan@users.noreply.github.com>
2019-06-14 10:04:49 +0200

    Update emby.sh

--- scripts/upgrade/emby.sh
+++ scripts/upgrade/emby.sh
@@ -15,3 +15,28 @@ echo ${username}
 systemctl disable emby-server --now
 chown -R $username:$username /var/lib/emby
 systemctl enable emby-server@$username --now
+
+#username=$(cat /root/.master.info | cut -d: -f1)
+#echo 'starting...'
+#sleep 10
+#echo "emby user changing to "${username}" "
+#systemctl disable emby-server --now
+#echo 'emby service disabled...'
+#sleep 5
+#chown -R $username:$username /var/lib/emby
+#echo 'permissions changed'
+#systemctl enable emby-server@$username --now
+#echo 'service started'
+
+#echo 'Stopping emby service...'
+#service emby-server stop
+#echo 'change emby username'
+#username=$(cat /root/.master.info | cut -d: -f1)
+#sed -i 's/.*setuid.*/setuid '$username'/' /etc/init/emby-server.conf
+#chown -R $username:$username /var/lib/emby
+#echo 'permissions changed'
+#service emby-server start
+#echo 'service started...'
+
+#apt-get install acl
+#setfacl -m user:emby:rx /home/<usuario>
