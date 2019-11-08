8d265baf81e2885f307e39642f705f3724d08e42
ajvulcan <ajteleco@gmail.com>
2019-11-08 20:51:35 +0100

    Algunos arreglos varios

---
+++ scripts/remove/sickchill.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Uninstaller for sickchill
+#
+# SERVIDOR HD 2019
+#
+user=$(cut -d: -f1 < /root/.master.info)
+systemctl disable sickchill@${user}
+systemctl stop sickchill@${user}
+sudo rm /etc/nginx/apps/sickchill.conf > /dev/null 2>&1
+sudo rm /etc/systemd/sickchill@.service > /dev/null 2>&1
+sudo rm /install/.sickchill.lock
+service nginx force-reload
+rm -rf /home/$user/.sickchill
+rm -f /install/.sickchill.lock
