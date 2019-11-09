0c1d66e03d83d0b20d4bdfffe0e6197d13c93131
ajvulcan <ajteleco@gmail.com>
2019-11-09 02:09:50 +0100

    Actualización de la configuración de nextcloud

--- scripts/install/nextcloud.sh
+++ scripts/install/nextcloud.sh
@@ -4,8 +4,9 @@
 #
 # Author:   liara for QuickBox.io
 # Ported to swizzin by liara
+# Forked to SERVIDOR HD by ajvulcan
 #
-# QuickBox Copyright (C) 2017 QuickBox.io
+# SERVIDOR HD 2019
 # Licensed under GNU General Public License v3.0 GPL-3 (in short)
 #
 #   You may copy, distribute and modify the software as long as you track
@@ -107,11 +108,18 @@ else
 fi
 
 cat > /etc/nginx/apps/nextcloud.conf <<EOF
+# The following 2 rules are only needed for the user_webfinger app.
+# Uncomment it if you're planning to use this app.
+#rewrite ^/.well-known/host-meta /nextcloud/public.php?service=host-meta last;
+#rewrite ^/.well-known/host-meta.json /nextcloud/public.php?service=host-meta-json last;
+# The following rule is only needed for the Social app.
+# Uncomment it if you're planning to use this app.
+#rewrite ^/.well-known/webfinger /nextcloud/public.php?service=webfinger last;
 location = /.well-known/carddav {
-  return 301 \$scheme://\$host/nextcloud/remote.php/dav;
+  return 301 \$scheme://\$host:\$server_port/nextcloud/remote.php/dav;
 }
 location = /.well-known/caldav {
-  return 301 \$scheme://\$host/nextcloud/remote.php/dav;
+  return 301 \$scheme://\$host:\$server_port/nextcloud/remote.php/dav;
 }
 
 location /.well-known/acme-challenge { }
@@ -135,61 +143,70 @@ location ^~ /nextcloud {
     #pagespeed off;
 
     location /nextcloud {
-        rewrite ^ /nextcloud/index.php\$uri;
+        rewrite ^ /nextcloud/index.php;
     }
 
-    location ~ ^/nextcloud/(?:build|tests|config|lib|3rdparty|templates|data)/ {
+    location ~ ^\/nextcloud\/(?:build|tests|config|lib|3rdparty|templates|data)\/ {
         deny all;
     }
-    location ~ ^/nextcloud/(?:\.|autotest|occ|issue|indie|db_|console) {
-        deny all;
+    location ~ ^\/nextcloud\/(?:\.|autotest|occ|issue|indie|db_|console) {        deny all;
     }
 
-    location ~ ^/nextcloud/(?:index|remote|public|cron|core/ajax/update|status|ocs/v[12]|updater/.+|ocs-provider/.+)\.php(?:$|/) {
-        fastcgi_split_path_info ^(.+\.php)(/.*)\$;
+    location ~ ^\/nextcloud\/(?:index|remote|public|cron|core\/ajax\/update|status|ocs\/v[12]|updater\/.+|oc[ms]-provider\/.+)\.php(?:\$|\/) {
+        fastcgi_split_path_info ^(.+?\.php)(\/.*|)\$;
+        set \$path_info \$fastcgi_path_info;
+        try_files \$fastcgi_script_name =404;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        fastcgi_param PATH_INFO \$fastcgi_path_info;
+        fastcgi_param PATH_INFO \$path_info;
         fastcgi_param HTTPS on;
-        #Avoid sending the security headers twice
+        # Avoid sending the security headers twice
         fastcgi_param modHeadersAvailable true;
+        # Enable pretty urls
         fastcgi_param front_controller_active true;
         fastcgi_pass unix:/run/php/$sock.sock;
         fastcgi_intercept_errors on;
         fastcgi_request_buffering off;
     }
 
-    location ~ ^/nextcloud/(?:updater|ocs-provider)(?:\$|/) {
+    location ~ ^\/nextcloud\/(?:updater|oc[ms]-provider)(?:\$|\/) {
         try_files \$uri/ =404;
         index index.php;
     }
 
-    # Adding the cache control header for js and css files
+    # Adding the cache control header for js, css and map files
     # Make sure it is BELOW the PHP block
-    location ~ \.(?:css|js|woff|svg|gif)\$ {
-        try_files \$uri /nextcloud/index.php\$uri\$is_args\$args;
+    location ~ ^\/nextcloud\/.+[^\/]\.(?:css|js|woff2?|svg|gif|map)\$ {
+        try_files \$uri /nextcloud/index.php\$request_uri;
         add_header Cache-Control "public, max-age=15778463";
         # Add headers to serve security related headers  (It is intended
         # to have those duplicated to the ones above)
         # Before enabling Strict-Transport-Security headers please read
         # into this topic first.
-        # add_header Strict-Transport-Security "max-age=15768000;
-        # includeSubDomains; preload;";
-        add_header X-Content-Type-Options nosniff;
-        add_header X-XSS-Protection "1; mode=block";
-        add_header X-Robots-Tag none;
-        add_header X-Download-Options noopen;
-        add_header X-Permitted-Cross-Domain-Policies none;
+        #add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;" always;
+        #
+        # WARNING: Only add the preload option once you read about
+        # the consequences in https://hstspreload.org/. This option
+        # will add the domain to a hardcoded list that is shipped
+        # in all major browsers and getting removed from this list
+        # could take several months.
+        add_header Referrer-Policy "no-referrer" always;
+        add_header X-Content-Type-Options "nosniff" always;
+        add_header X-Download-Options "noopen" always;
+        add_header X-Frame-Options "SAMEORIGIN" always;
+        add_header X-Permitted-Cross-Domain-Policies "none" always;
+        add_header X-Robots-Tag "none" always;
+        add_header X-XSS-Protection "1; mode=block" always;
+
         # Optional: Don't log access to assets
         access_log off;
     }
-
-    location ~ \.(?:png|html|ttf|ico|jpg|jpeg)\$ {
-        try_files \$uri /nextcloud/index.php\$uri\$is_args\$args;
-        # Optional: Don't log access to other assets
+    location ~ ^\/nextcloud\/.+[^\/]\.(?:png|html|ttf|ico|jpg|jpeg|bcmap)\$ {
+        try_files \$uri /nextcloud/index.php\$request_uri;
+       # Optional: Don't log access to other assets
         access_log off;
     }
-  }
+}
 EOF
 
 mysql --user="root" --password="$password" --execute="CREATE DATABASE nextcloud;"
