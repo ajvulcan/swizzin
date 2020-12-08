9339ad7cf234acd016f68823d2838644c267f376
root <root@ns371165.ip-5-39-64.eu>
2020-12-08 04:01:17 +0000

    Añadiendo seguridad a librespeed

--- scripts/nginx/librespeed.sh
+++ scripts/nginx/librespeed.sh
@@ -7,9 +7,12 @@
 if [[ ! -f /etc/nginx/apps/librespeed.conf ]]; then
 	cat > /etc/nginx/apps/librespeed.conf <<RAP
 location /librespeed {
-	alias /srv/librespeed;
-	client_max_body_size 50M;
-	client_body_buffer_size 128k;
+  alias /srv/librespeed;
+  client_max_body_size 50M;
+  client_body_buffer_size 128k;
+
+  auth_basic "What's the password?";
+  auth_basic_user_file /etc/htpasswd;
 }
 RAP
 fi
